9c36fcffc72bb1eb59eacb3f936a3c45175b466f
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2020-12-17 05:58:27 +0000

    First init

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'f29b3838b9190b10e7b7c666a46eb960d7be9902883e71a147ae18e4e4a60fb1e983f012c4cd5b6752c474bf84fdfd24b42159cdbddbca46a45b6d856b6288fa'
+
