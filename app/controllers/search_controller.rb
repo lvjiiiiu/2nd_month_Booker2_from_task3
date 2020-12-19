111848ac97e60b372eab4e8abb6753a101e49126
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2020-12-19 01:31:23 +0000

    [Add]検索フォーム

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,55 @@
 class SearchController < ApplicationController
   def search
+    @value = params['search']['value']
+    @how = params['search']['how']
+    @model = params['search']['model']
+    @datas = search_for(@how, @model, @content)
+  end
+  
+  private
+
+  def match(model, value)                     #def search_forでhowがmatchだった場合の処理
+    if model == 'user'                        #modelがuserの場合の処理
+      User.where(name: value)                 #whereでvalueと完全一致するnameを探します
+    elsif model == 'book'       
+      Book.where(title: value)
+    end
+  end
+
+  def forward(model, value)
+    if model == 'User'
+      User.where("name LIKE ?", "#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "#{value}%")
+    end
+  end
+
+  def backward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}")
+    end
+  end
+
+  def partical(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}%")
+    end
+  end
+
+  def search_for(how, model, value)　　　　#searchアクションで定義した情報が引数に入っている
+    case how                              #検索方法のhowの中身がどれなのかwhenの条件分岐の中から探す処理
+    when 'match'
+      match(model, value)                 #検索方法の引数に(model, value)を定義している
+    when 'forward'                        #例えばhowがmatchの場合は def match の処理に進みます
+      forward(model, value)
+    when 'backward'
+      backward(model, value)
+    when 'partical'
+      partical(model, value)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  
-  get 'search/search'
+
 devise_for :users
   root to: 'home#top'
   get "home/about" => "home#about"
+  
   resources :books, except: [:new] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end 
-  
   
   resources :users, except: [:new, :destroy, :create] do
     resource :relationships, only:[:create, :destroy] 
     get '/follows' => 'relationships#follower'
     get '/followers' => 'relationships#followed'
   end
+  
+  get '/search' => 'search#search'
+  
 end
