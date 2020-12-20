eb051a77ffca0045019df4d022f369e0988fd870
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2020-12-20 11:41:55 +0000

    コメント機能の非同期化おおよそ完成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,22 @@
 class BookCommentsController < ApplicationController
-  
-  def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
-  end
-  
-  def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
-  end
-  
-private
-  
-  def book_comment_params
-    params.require(:book_comment).permit(:comment)
-  end
+	before_action :authenticate_user!
+
+	def create
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		@book_comment.save
+	end
+
+	def destroy
+		@book = Book.find(params[:book_id])
+  		book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+	end
+
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
 end
