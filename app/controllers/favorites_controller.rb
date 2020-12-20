10bc812609d61beee3197e5dedabc7bc5901d8b2
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2020-12-20 09:00:46 +0000

    いいね機能の非同期通信化完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,14 @@
 class FavoritesController < ApplicationController
-    
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back fallback_location: books_path 
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back fallback_location: books_path
   end
-  
-  
 end
 
